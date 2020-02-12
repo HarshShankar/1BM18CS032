42fe8324ec9d8af6bc9b2056357cf18895eb8daa
Harsh Shankar <harshshankar.cs18@bmsce.ac.in>
2020-02-12 22:19:48 +0530

    Updated bi

--- 4thSEM/ADA/bi.py
+++ 4thSEM/ADA/bi.py
@@ -1,26 +1,31 @@
-def first(a,beg,end,key):
-	if(beg<=end):
-		mid=(beg+end)/2
-		if((mid==0 or x > a[mid-1]) and x==a[mid]):
-			return mid
-		elif(x>a[mid]):
-			return first(a,mid+1,end,key)
-		else:
-			return first(a,beg,mid-1,key)
-	return -1
-	
-def last(a,beg,end,key):
-	if(beg<=end):
-		mid=(beg+end)/2
-		if((mid== len(arr)-1 or x < a[mid-1]) and x==a[mid]):
-			return mid
-		elif(x < a[mid]):
-			return first(a,beg,mid+1,key)
-		else:
-			return first(a,mid+1,end,key)
-	return -1
+def first(arr,beg,end,x):
+    if(beg<=end):
+        mid=(beg+end)//2
+        if((mid==0 or arr[mid-1]<x)and arr[mid]==x):
+            return mid
+        elif(arr[mid]<x):
+            return first(arr,mid+1,end,x)
+        else:
+            return first(arr,beg,mid-1,x)
+    else:
+        return -1
 
-	
-	
-arr=[1,1,1,4,5]
-binary(arr,0,len(arr)-1,1)			 
+def last(arr,beg,end,x):
+    if(beg<=end):
+        mid=(beg+end)//2
+        if((x==end or arr[mid+1]>x) and arr[mid]==x):
+            return mid
+        elif(x<arr[mid]):
+            return last(arr,beg,mid-1,x)
+        else:
+            return last(arr,mid+1,end,x)
+    else:
+        return -1
+
+ar=[1,2,2,2,2,3,4,5]
+fir=first(ar,0,len(ar)-1,2)
+las=last(ar,0,len(ar)-1,2)
+if(fir!=-1):
+    print("first occurence=",fir,"\nlast occurence=",las)
+else:
+    print("Element not found")
