58fa8f288b20461aea21f52cc6d7cbe15564970d
Harsh Shankar <harshshankar.cs18@bmsce.ac.in>
2020-03-31 17:05:50 +0530

    Update quicksort.py

--- 4thSEM/ADA/quicksort.py
+++ 4thSEM/ADA/quicksort.py
@@ -1,1 +1,27 @@
+import time
+def partition(arr, low, high):
+    i = (low-1)
+    pivot = arr[high]
 
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+
+def quickSort(arr, low, high):
+    if low < high:
+        part = partition(arr, low, high)
+        quickSort(arr, low, part-1)
+        quickSort(arr, part+1, high)
+
+arr = [int(i) for i in input("").split()]
+n = len(arr)
+start = time.time()
+quickSort(arr, 0, n-1)
+end = time.time()
+print("Sorted array is: ", end="")
+print(arr)
+print("Time taken : " + str(end-start))
