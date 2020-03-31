7fa59040971815f8254ce1464f1f51e17deb615c
Harsh Shankar <harshshankar.cs18@bmsce.ac.in>
2020-03-31 17:03:08 +0530

    done

--- 4thSEM/ADA/xor.py
+++ 4thSEM/ADA/xor.py
@@ -1,1 +1,19 @@
+  
+def find(n):
+    arr = [[0 for h in range(n)]
+           for b in range(n)]
+    x = 0
+    for i in range(n // 4):
+        for j in range(n // 4):
+            for h in range(4):
+                for b in range(4):
+                    arr[i * 4 + h][j * 4 + b] = x
+                    x += 1
+    for i in range(n):
+        for j in range(n):
+            print(arr[i][j], end=" ")
+        print()
 
+
+n = int(input('Enter n: '))
+find(n)
