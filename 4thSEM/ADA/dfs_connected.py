7b4a2540c7de8c715900bfa2ae7946fc6e3615da
Harsh Shankar <harshshankar.cs18@bmsce.ac.in>
2020-05-12 22:06:24 +0530

    Create dfs_connected.py

---
+++ 4thSEM/ADA/dfs_connected.py
@@ -0,0 +1,31 @@
+ 
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+n = int(input("Enter number of vertices: "))
+e = int(input("Enter the number of edges: "))
+if(n == 0 or e == 0):
+    print('Order-zero graph!')
+    exit()
+graph = {}
+print("Enter the edges in the format [start] [end]")
+for i in range(e):
+    start, end = list(input("").split())
+    if not graph.get(start):
+        graph[start] = set([end])
+    else:
+        graph[start].add(end)
+    if not graph.get(end):
+        graph[end] = set([start])
+    else:
+        graph[end].add(start)
+
+if len(dfs(graph, list(graph.keys())[0])) == n:
+    print("Connected")
+else:
+    print("Not connected")
