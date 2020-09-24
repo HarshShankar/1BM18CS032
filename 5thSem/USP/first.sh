c201338474434c59cc66de84043aa578d74b30e1
HarshShankar <harshshankar.cs18@bmsce.ac.in>
2020-09-24 15:15:27 +0530

    unix lab-1

---
+++ 5thSem/USP/first.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Today's date: `date` "
+echo "My shell: $SHELL"
+echo "This month's calender: \n `cal`"
+echo "Today's date: `date "+%d%m%Y"`"
---
+++ 5thSem/USP/oddeven.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Enter two numbers"
+read a
+read b
+sum=$(($a+$b))
+sub=$(($a-$b))
+mul=$(($a*$b))
+div=$(($a/$b))
+rem=$(($a%$b))
+echo $sum
+echo $sub
+echo $mul
+echo $div
+echo $rem
